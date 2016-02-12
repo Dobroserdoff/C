97f1e94b71658e967afcc1ac2b719a34d4a50a91
Dobroserdoff <igel@bk.ru>
2016-02-12 15:56:12 +0500

    Entab with arguments

---
+++ tutorial/chapter005/011/entab.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LENGTH 4
+
+int main(int argc, char *argv[]) {
+    int n = 0, i, c, b = 0, cur, k = 1;
+    cur = 1;
+
+    while ((c = getchar()) != EOF) {
+        if (k < argc && n++ == (atoi(argv[k]))) {
+            putchar('\t');
+            k++;
+        }
+        
+        if (c != ' ') {
+            if (cur > 1 && cur < LENGTH)
+                for (i = 0; i < cur; i++)
+                    putchar(' ');
+            
+            else if (cur >= LENGTH) {
+                for (i = cur/LENGTH; i > 0; i--)
+                    putchar('\t');
+                
+                for (i = cur%LENGTH; i > 0; i--)
+                    putchar(' ');
+            }
+            putchar(c);;
+            b = 0;
+            cur = 1;
+        }
+        
+        else
+            if (b == 0)
+                b = 1;
+            else
+                cur++;
+    }
+    return 0;
+}
