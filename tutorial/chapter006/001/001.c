38bc470f182531113ed0b585108ac3355dec29a4
Dobroserdoff <igel@bk.ru>
2016-03-03 21:22:10 +0500

    Exercise 6-1 done

--- tutorial/chapter006/001/001.c
+++ tutorial/chapter006/001/001.c
@@ -9,6 +9,8 @@ struct key {
     char *word;
     int count;
 } keytab[] = {
+    "#include", 0,
+    "_char", 0,
     "auto", 0,
     "break", 0,
     "case", 0,
@@ -18,12 +20,14 @@ struct key {
     "default", 0,
     "else", 0,
     "if", 0,
+    "include", 0,
     "int", 0,
     "unsigned", 0,
     "void", 0,
     "volatile", 0,
     "while", 0
 };
+int flag = 0;
 int bufp = 0;
 char buf[BUFSIZE];
 
@@ -38,7 +42,7 @@ int main() {
     int NKEYS = sizeof(keytab) / sizeof(struct key);
 
     while (getword(word, MAXWORD) != EOF)
-        if (isalpha(word[0]))
+        if (isalpha(word[0]) || (word[0] == '_') || (word[0] == '#'))
             if ((n = bsearch(word, keytab, NKEYS)) >= 0)
                 keytab[n].count++;
     
@@ -61,29 +65,53 @@ void ungetch(int c) {
 }
 
 int getword(char *word, int lim) {
-    int c, getch(void);
-    void ungetch(int);
+    int c;
     char *w = word;
 
     while (isspace(c = getch()))
-        ;
+        if ((c == '\n') && (flag == 3)) 
+            flag = 0;
+       
+    if (c == '"') {
+        if (flag == 0)
+            flag = 1;
+        else if (flag == 1)
+            flag = 0;
+    }
+
+    if ((c == '/') && (flag == 0)) {
+        c = getch();
+        if (c == '*')
+            flag = 2;
+        else if (c == '/')
+            flag = 3;
+        else
+            ungetch(c);
+    }
+
+    if ((c == '*') && (flag == 2))
+        ((c = getch()) == '/') ? (flag = 0) : (ungetch(c));
+
 
     if (c != EOF)
         *w++ = c;
     
-    if (!isalpha(c)) {
+    if ((!isalpha(c)) && (c != '_') && (c != '#')) {
         *w = '\0';
         return c;
     }
     
     for (; --lim > 0; w++)
-        if (!isalnum(*w = getch())) {
+        if ((!isalnum(*w = getch())) && (*w != '_') && (*w != '#')) {
             ungetch(*w);
             break;
         }
     
     *w = '\0';
-    return word[0];
+    if (flag == 0)
+        return word[0];
+    else
+        return word[0] = '\0';
 }
 
 int bsearch(char *word, struct key tab[], int n) {
