6ea3600434ee3833139cb5302854e9ae6c49fe1c
Dobroserdoff <igel@bk.ru>
2016-05-24 21:19:34 +0300

    Speedtests done needs some visuals

--- multithread/pool/main.c
+++ multithread/pool/main.c
@@ -2,21 +2,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int par[3] = { 1, 2, 4 };
 int asr[3] = { 100000, 500000, 1000000 };
 int rr[3] = { 0, 100, 1000 };
 int br[3] = { 0, 10, 100 };
-char buf[120];
 char* path = { "./pool" };
-char* arg1 = { " -p " };
-char* arg2 = { " -a " };
-char* arg3 = { " -r " };
-char* arg4 = { " -b " };
+char* arg1 = { "-p" };
+char* arg2 = { "-a" };
+char* arg3 = { "-r" };
+char* arg4 = { "-b" };
 
 void gettime(char* const*);
-void itoa(int, char*);
+void itoa(int, char**);
 
 int main() {
     int i, j, k, l;
@@ -26,73 +28,85 @@ int main() {
     char* sep2 = { "----------------------------------------" };
     char** argums;
     char** startarg;
+
+    startarg = malloc(100 * sizeof(void*));
+    argums = startarg;
+    *argums++ = path;
+    *argums++ = arg1;
     
-    startarg = malloc(sizeof(void*) * 10);
-    
-    for (i = 0; i < (sizeof(par) / 4); i++) {
-        argums = startarg;
-        *argums++ = path;
-        *argums++ = arg1;
-        itoa(par[i], *argums++);
-       
-        for (j = 0; j < (sizeof(asr) / 4); j++) {
-            *argums++ = arg2;
-            itoa(asr[j], *argums++);
+    for (i = 0; i < (sizeof(par) / sizeof(int)); i++) {
+        printf("i - %d, ", i);
+        itoa(par[i], argums++);
+        *argums++ = arg2;
+        
+        for (j = 0; j < (sizeof(asr) / sizeof(int)); j++) {
+            printf("j - %d, ", j);
+            itoa(asr[j], argums++);
+            *argums++ = arg3;
             
-            for (k = 0; k < (sizeof(rr) / 4); k++) {
-                *argums++ = arg3;
-                itoa(rr[k], *argums++);
-
+            for (k = 0; k < (sizeof(rr) / sizeof(int)); k++) {
+                printf("k - %d, ", k);
+                itoa(rr[k], argums++);
+                *argums++ = arg4;
+                
                 for (l = 0; l <= k; l++) {
-                    *argums++ = arg4;
-                    itoa(br[l], *argums++) ;
-                    
-                    gettime(startarg);
-                }
+                    printf("l - %d\n", l);
+                    itoa(br[l], argums++) ;
+                    *argums-- = NULL;
 
+                    gettime(startarg);
+                    free(*argums);
+                }
+                argums -= 2;
+                free(*argums);
                 if (j != (sizeof(asr) / 4) - 1) {
-                    puts(sep2);
+                    //puts(sep2);
                 }
             }
-
-            puts(sep1);  
-        }
-
-        free(startarg);
-        return 0;
+            argums -= 2;
+            free(*argums);
+            //puts(sep1);  
+        }   
+        argums -= 2;
+        free(*argums);
     }
+    free(startarg);
+        
+    return 0;
 }
 
 void gettime(char* const* arg) {
+    int err;
     pid_t pid;
     struct timeval begin, end;
     
+    if (gettimeofday(&begin, 0) == 0) {
+        //printf("BSEC - %d, BMSEC - %d\n", (int) begin.tv_sec, (int) begin.tv_usec);
+    }
+
     pid = fork();
-    if (pid == 0) {
-        if (gettimeofday(&begin, 0) == 0) {
-            printf("BSEC - %d, BMSEC - %d/n", (int) begin.tv_sec, (int) begin.tv_usec);
-        }
-
+    if (pid == 0) {    
         execv(path, arg);
-    
-        if (gettimeofday(&end, 0) == 0) {
-            printf("ESEC - %d, EMSEC - %d/n", (int) end.tv_sec, (int) end.tv_usec);
-        }
-
+        puts(strerror(errno));
     }
 
     if (pid > 0) {
         waitpid(pid, NULL, 0);
+        if (gettimeofday(&end, 0) == 0) {
+            //printf("ESEC - %d, EMSEC - %d\n", (int) end.tv_sec, (int) end.tv_usec);
+        }
     }
     
-    //printf("%10d%10d%10d%10d%10ld\n", p, a, r, b);
+    //printf("Seconds - %ld, microseconds - %ld\n", end.tv_sec - begin.tv_sec, end.tv_usec - begin.tv_usec);
 
 }
 
-void itoa(int n, char* buf) {
+void itoa(int n, char** buf) {
     int c, j, i, sign;
     char *s;
-    s = buf;
+
+    s = (char*)malloc(11*(sizeof(char)));
+    *buf = s;
 
     if ((sign = n) < 0)
         n = -n;
@@ -101,14 +115,12 @@ void itoa(int n, char* buf) {
     }
     while ((n /= 10) > 0);
         
-    if (sign < 0)
-        *s++ = '-';
-    
     *s = '\0';
-    
-    for (i = 0, j = strlen(buf)-1; i < j; i++, j--) {
-        c = buf[i];
-        buf[i] = buf[j];
-        buf[j] = c;
+    s = *buf;
+
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
     }
 }
